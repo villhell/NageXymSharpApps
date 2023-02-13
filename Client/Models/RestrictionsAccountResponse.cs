bea1fe780e3b949cebdb9004285f216cb62e1384
villhell <lilly.of.the.valley.to@gmail.com>
2023-02-14 08:25:10 +0900

    アドレス制限を確認する処理を追加

---
+++ Client/Models/RestrictionsAccountResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace NageXymSharpApps.Client.Models
+{
+    public class RestrictionsAccountResponse
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+    }
+}
