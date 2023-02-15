07235f333b89c89aaee1198e30b3a16349b14e50
villhell <lilly.of.the.valley.to@gmail.com>
2023-02-15 22:13:01 +0900

    SSSと連携、まだ動作不良あり

---
+++ Client/NpmJS/src/index.js
@@ -0,0 +1,19 @@
+﻿import { getActiveNode } from "symbol-node-util";
+import { SSSWindow, setTransactionByPayload, requestSign, getActiveAddress } from "sss-module";
+
+export async function GetActiveNode(network) {
+    return await getActiveNode(network);
+}
+
+export async function SetTransactionByPayload(payload) {
+    setTransactionByPayload(payload);
+    return await requestSign();
+}
+
+export async function RequestSign() {
+    return await requestSign();
+}
+
+export function GetActiveAddress() {
+    return getActiveAddress();
+}
