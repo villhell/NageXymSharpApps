e6fad6fd471601f0dd1a53fca019b5ea464a851a
villhell <lilly.of.the.valley.to@gmail.com>
2023-03-09 20:09:45 +0900

    add sound hidden twitter uri

--- Client/wwwroot/js/play-sound.js
+++ Client/wwwroot/js/play-sound.js
@@ -1,8 +1,10 @@
-﻿
+﻿let start_sound = new Audio("/audio/ding.ogg");
+let end_sound = new Audio("/audio/ding2.ogg");
+
 export function start() {
-    new Audio("/audio/ding.ogg");
+    start_sound.play();
 }
 
 export function end() {
-    new Audio("/audio/ding2.ogg");
+    end_sound.play();
 }
